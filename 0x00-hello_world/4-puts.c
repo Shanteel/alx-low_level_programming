8ff2e0e9ca4ea59257fb9b966eb35d0f2a762d1f
Shanteel <taushanteelmm@gmail.com>
2023-02-16 15:31:11 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A c program that prints a line using the fucntion puts
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
