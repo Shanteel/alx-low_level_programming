5ebd8256767ed03972247e16c7ba5dd241a9bb97
Shanteel <taushanteelmm@gmail.com>
2023-02-16 15:45:12 -0800

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A c program that prints a line exactly using the function printf
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
