4d72e3bacba289ebef417a32e0edcd8ac1e50b44
Shanteel <taushanteelmm@gmail.com>
2023-02-18 17:01:06 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A program that prints digits in singular form
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 0) {
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
