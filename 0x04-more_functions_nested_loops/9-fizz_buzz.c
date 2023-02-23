a7bccab290123a0643bfbc08cabc360afff83c75
Shanteel <taushanteelmm@gmail.com>
2023-02-23 13:54:53 -0800

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print a Fizz Buzz program
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 1; c <= 100; c++)
+	{
+	if ((c % 3 == 0) && (c % 5 == 0))
+	{
+	printf("FizzBuzz");
+	}
+	else if (c % 3 == 0)
+	{
+	printf("Fizz");
+	}
+	else if (c % 5 == 0)
+	{
+	printf("Buzz");
+	}
+	else
+	{
+	printf("%d", c);
+	}
+	if (c != 100)
+	{
+	printf(" ");
+	}
+	}
+	printf("\n");
+	return (0);
+}
+
