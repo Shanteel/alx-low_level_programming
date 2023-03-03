116c9ce73aaa40bd54fb269a9ba3c557781017e1
Shanteel <taushanteelmm@gmail.com>
2023-03-02 16:59:17 -0800

    Task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,11 +6,23 @@
  * @s: pointer to string params
  * Return: *S
  */
-char *rot13(char *S)
+char *rot13(char *s)
 {
 	int i;
 	int j;
 
 	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	for (
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	for (j = 0; j < 52; j++)
+	{
+	if (s[i] == data1[j])
+	{
+	s[i] = datarot[j];
+	break;
+	}
+	}
+	}
+	return (s);
+}
