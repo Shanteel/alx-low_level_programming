4705bab8e7b33322302b5e598b685f8f26be10c1
Shanteel <taushanteelmm@gmail.com>
2023-03-08 13:27:48 -0800

    Task 0

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - Writes the c character to the Standardout
+ * @c: charater to print
+ *
+ * Return: 1 (success)
+ * on error, -1 is returned, and errno is set appropriately
+ */
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
