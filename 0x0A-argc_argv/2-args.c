404fb6fae402caa2eb49b82889c09bd2cb63ae88
Shanteel <taushanteelmm@gmail.com>
2023-03-12 21:56:02 -0700

    Task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,19 +11,14 @@
  */
 int main(int argc, char *argv[])
 {
-	int n1 = 0, n2 = 0;
+	int n;
 
-	if (argc == 3)
+	for (n = 0; n < argc; n++)
 	{
-		n1 = atoi(argv[1]);
-		n2 = atoi(argv[2]);
-		printf("%d\n", n1 * n2);
-	}
-	else
-	{
-		printf("Error\n");
-		return (1);
+		printf("%s\n", argv[n]);
 	}
 
 	return (0);
 }
+			
+
